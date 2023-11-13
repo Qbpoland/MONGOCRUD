2ef99261d2283d409815cfdad66c244625bc3642
Qbpoland <jakub.stasiuk2005@gmail.com>
2023-11-13 13:44:15 +0100

    del add itp

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectId} = require('mongodb');
 const uri = "mongodb+srv://Admin:Admin12@cluster0.x1bcj5q.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 
 
@@ -21,8 +21,24 @@ async function getAllListings(client) {
     let list = collection.find().toArray();
     return list;
 }
+async function get(client, criteria) {
+    const collection = await client.db("sample_airbnb").collection("listingsAndReviews");
+    let list = collection.find(criteria).toArray();
+    return list;
+}
+    async function add(client, data) {
+
+        const collection = await client.db("sample_airbnb").collection("listingsAndReviews");       
+        collection.insertOne(data);
+
+    }
+    async function del(client, partId) {
+       let r = await client.db("sample_airbnb").collection("listingsAndReviews").deleteOne({_id:partId});
+       return r;
+    }
+
     function close(client) {
         client.close();
     }
 
-module.exports = {connect, getAllListings, close}
+module.exports = {connect, getAllListings, close, get, add, del};
--- index.js
+++ index.js
@@ -1,32 +1,67 @@
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const db = require('./db');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', async (req, res) => {
-res.send('Hello World!');
+    res.send('Hello World!');
 
 });
 
 app.get('/listAll', async (req, res) => {
-res.write("<h1> List All </h1>");
+    res.write("<h1> List All </h1>");
+    const client = await db.connect();
+    res.write("<table>");
+    let list = await db.getAllListings(client);
+    list.forEach(element => {
+        res.write("<tr>");
+        res.write("<td>" + element.listing_url + "</td>");
+        res.write("<td>" + element.name + "</td>");
+        res.write("<tr>");
+    });
+    res.write("</table>");
+    db.close(client);
+    res.end();
+});
+
+app.post('/list', async (req, res) => {
+    //console.log(req.body);
+    //res.sendStatus(200);
+    res.contentType('text/html');
+    res.write("<h1> List </h1>");
+    res.write("<h1> List All </h1>");
+    const client = await db.connect();
+    res.write("<table>");
+    let list = await db.get(client, req.body);
+    list.forEach(element => {
+        res.write("<tr>");
+        res.write("<td>" + element.listing_url + "</td>");
+        res.write("<td>" + element.name + "</td>");
+        res.write("<tr>");
+    });
+    res.write("</table>");
+    db.close(client);
+    res.end();
+
+
+});
+
+
+app.post('/add', async (req, res) => {
+console.log(req.body);
 const client = await db.connect();
-res.write("<table>");
-let list = await db.getAllListings(client);
-list.forEach(element => {
-    res.write("<tr>");
-    res.write("<td>" + element.listing_url + "</td>");
-    res.write("<td>" + element.name + "</td>");
-    res.write("<tr>");
-});
-res.write("</table>");
-db.close(client);
-res.end();
+db.add(client, req.body);
+
 });
 
-
-
-
+app.post('/del', async (req, res) => {
+const client = await db.connect(); 
+let r = await db.del(client, req.body.id);
+res.send(r);
+});
 app.listen(8000, () => {
-console.log('Example app listening on port 8000!');
+    console.log('Example app listening on port 8000!');
 });
